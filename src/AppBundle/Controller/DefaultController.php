7be08131b9e795605eb640ee04e0608bb1a4639f
Dusan Babic <dusan.babic@labs.devana.rs>
2016-07-17 22:56:06 +0200

    test 2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,13 @@
 namespace AppBundle\Controller;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use GuzzleHttp\Pool;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UriInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,15 +36,49 @@ class DefaultController extends Controller
      */
     public function ajaxUrl(Request $request)
     {
-        $urlsDone = 0;
-        $urls      = $request->request->get('urls');
-        $initNum = $request->request->get('initNum');
-        $urlsArray = explode(",", $urls);
-        $urlsNum = count($urlsArray);
+//        $stack = new HandlerStack();
+//        $stack->setHandler(new CurlHandler());
+//        $stack->push(Middleware::redirect());
+//        $client = new Client(['handler' => $stack]);
         $client    = new Client();
-        $requests  = function ($urlsArray) {
+        $urlsDone  = 0;
+        $urls      = $request->request->get('urls');
+        $initNum   = $request->request->get('initNum');
+        $urlsArray = explode(",", $urls);
+        $urlsNum   = count($urlsArray);
+
+        $onRedirect = function (    RequestInterface $request,
+                                    ResponseInterface $response,
+                                    UriInterface $uri) use ($urlsDone, $urlsArray, $initNum, $urlsNum){
+//            global $urlsDone;
+//            $urlsDone = $urlsDone + 1;
+            $status   = $response->getStatusCode();
+            $length   = $response->getBody()->getSize();
+            echo "<p>";
+            echo '*'.$initNum.'* ';
+            echo "<b> Redirecting from: </b>".(string)$request->getUri()."<b> to: </b>".$uri." <b> status: </b>".$status." <b> length: </b>".$length."</p>";
+            echo PHP_EOL;
+//            echo 'Redirecting! ' . $request->getUri() . ' to ' . $uri . "\n";
+//            $var = "RADII!!";
+//            var_dump($var);
+//            dump($var);
+//            die();
+        };
+
+        $requests = function ($urlsArray) use ($onRedirect) {
             foreach ($urlsArray as $url) {
-                yield new \GuzzleHttp\Psr7\Request('GET', $url);
+                yield new \GuzzleHttp\Psr7\Request('GET', $url
+//                    [
+//                    'allow_redirects' => [
+//                        'max'             => 10,        // allow at most 10 redirects.
+//                        'strict'          => true,      // use "strict" RFC compliant redirects.
+//                        'referer'         => true,      // add a Referer header
+//                        'protocols'       => ['https'], // only allow https URLs
+//                        'on_redirect'     => $onRedirect,
+//                        'track_redirects' => true,
+//                    ]
+//                ]
+                );
             }
         };
 
@@ -50,28 +90,50 @@ class DefaultController extends Controller
         $response = new StreamedResponse();
         $response->headers->set('Content-Type', 'text/event-stream');
 
-        $pool    = new Pool($client, $requests($urlsArray), [
+        $pool = new Pool($client, $requests($urlsArray), [
             'concurency' => 1000,
-            'fulfilled'  => function ($response, $index) use ($urlsDone, $urlsArray, $initNum, $urlsNum){
+            'fulfilled'  => function ($response, $index) use ($urlsDone, $urlsArray, $initNum, $urlsNum) {
                 global $urlsDone;
-                sleep(2);
+                sleep(1);
+                if ($response->getHeaderLine('X-Guzzle-Redirect-History') != ""){
+                    $urlsDone = $urlsDone + 1;
+                    $status   = $response->getStatusCode();
+                    $length   = $response->getBody()->getSize();
+                    $redirectHistory = explode(',', $response->getHeaderLine('X-Guzzle-Redirect-History'));
+                    $redirectHistory = array_slice($redirectHistory, -1);
+                    $lastRedirect = array_pop($redirectHistory);
+
+//                    echo '******';
+//                    var_dump($response);
+                    echo "<p>";
+                    echo '*'.$initNum.'* ';
+//                    echo $response->getHeaderLine('X-Guzzle-Redirect-History');
+                    echo "<b> url: </b>".$lastRedirect." <b> status: </b>".$status."<b> length: </b>".$length." <b> urlsDone: </b>".$urlsDone.'/'.$urlsNum."</p>";
+                    echo PHP_EOL;
+                    ob_flush();
+                    flush();
+
+                }else {
 //                    $array = array("url" => $urls[$index], "status"=>$response->getStatusCode(), "length"=>$response->getBody()->getSize(), "parent"=>$parent, "index"=>$index);
-                $urlsDone = $urlsDone + 1;
-                $status = $response->getStatusCode();
-                $length = $response->getBody()->getSize();
+                    $urlsDone = $urlsDone + 1;
+                    $status   = $response->getStatusCode();
+                    $length   = $response->getBody()->getSize();
 //                    echo " sajt: ".$urlsArray[$index]." uspesno_".$index." status: ".$status."; *".$urlsDone."*";
-                //$responseData = array("url" => $urlsArray[$index], "status" => $status, "length" => $length, "urlsDone" => $urlsDone);
-                echo "<h6>";
-                echo '*'.$initNum.'* ';
-                echo "url: ".$urlsArray[$index]." status: ".$status." length: ".$length." urlsDone: ".$urlsDone.'/'.$urlsNum."</h6>";
-                echo PHP_EOL;
-                ob_flush();
-                flush();
+                    //$responseData = array("url" => $urlsArray[$index], "status" => $status, "length" => $length, "urlsDone" => $urlsDone);
+
+                    echo "<p>";
+                    echo '*'.$initNum.'* ';
+                    echo $response->getHeaderLine('X-Guzzle-Redirect-History');
+                    echo "<b>url: </b>".$urlsArray[$index]."<b> status: </b>".$status." <b> length: </b>".$length." <b> urlsDone: </b>".$urlsDone.'/'.$urlsNum."</p>";
+                    echo PHP_EOL;
+                    ob_flush();
+                    flush();
 //                    $response1->send();
 //                    var_dump($response);
+                }
 
             },
-            'rejected'   => function ($reason, $index) use ($urlsDone, $urlsNum, $urlsArray, $initNum){
+            'rejected'   => function ($reason, $index) use ($urlsDone, $urlsNum, $urlsArray, $initNum) {
                 global $urlsDone;
 //                    sleep(2);
                 $urlsDone = $urlsDone + 1;
@@ -83,7 +145,14 @@ class DefaultController extends Controller
 //                    $response1->send();
 //                    var_dump($reason);
 
-            }
+            },
+            'options'    => [
+                'allow_redirects' => [
+                    'max'             => 5,
+                    'on_redirect'     => $onRedirect,
+                    'track_redirects' => true
+                ]
+            ]
         ]);
 
         $response->setCallback(function () {
@@ -116,9 +185,37 @@ class DefaultController extends Controller
      */
     public function guzzleTest()
     {
-        $client         = new Client();
-        $guzzleResponse = $client->request('GET', 'https://www.google.rs/');
-        $guzzleResponse->getStatusCode();
+//        $client         = new Client();
+//        $guzzleResponse = $client->request('GET', 'https://www.google.rs/');
+//        $guzzleResponse->getStatusCode();
+//        var_dump($guzzleResponse);
+//        die();
+
+        function add_response_header($header, $value)
+        {
+            return function (callable $handler) use ($header, $value) {
+                return function (
+                    RequestInterface $request,
+                    array $options
+                ) use ($handler, $header, $value) {
+                    $promise = $handler($request, $options);
+                    return $promise->then(
+                        function (ResponseInterface $response) use ($header, $value) {
+                            return $response->withHeader($header, $value);
+                        }
+                    );
+                };
+            };
+        }
+
+        $stack = new HandlerStack();
+        $stack->setHandler(new CurlHandler());
+        $stack->push(add_response_header('X-Foo', 'bar'));
+        $stack->push(Middleware::redirect());
+        $client         = new Client(['handler' => $stack]);
+        $guzzleResponse = $client->request('GET', 'https://www.google.com/');
+        $xfoo           = $guzzleResponse->getHeader('X-Foo');
+        var_dump($xfoo);
         var_dump($guzzleResponse);
         die();
     }
